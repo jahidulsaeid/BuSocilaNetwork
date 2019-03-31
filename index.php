e8241ef4dbf7b5da83f3644b7965cffabe92e2cc
muhammadsaeid <muhammadsaeid0@gmail.com>
2019-03-30 21:55:40 -1200

    All File add

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bu Socila NetWork</title>
+    <!-- Semantic min Css -->
+    <link rel="stylesheet" href="./semantic.min.css">
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+ <?php include('./components/Header/header.php'); ?>
+
+    <!-- Jquery min Files -->
+    <script src="./Components/Dist/Jquery-3.1.1.min.js"></script>
+    <!-- Sematic Min Files -->
+    <script src="./semantic.min.js"></script>
+</body>
+</html>
