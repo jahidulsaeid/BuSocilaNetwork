7eb726529d71cfff45d4b2fed83480f750b461eb
muhammadsaeid <muhammadsaeid0@gmail.com>
2019-03-31 02:13:31 -1200

    Connection to Components

---
+++ Components/Header/Header.php
@@ -0,0 +1,1 @@
+<?php set_include_path("./../../index.php"); ?>
--- index.php
+++ index.php
@@ -1,21 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bu Socila NetWork</title>
     <!-- Semantic min Css -->
-    <link rel="stylesheet" href="./semantic.min.css">
+    <link rel="stylesheet" href="./Components/Dist/semantic.min.css">
     <!-- Custom Css -->
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
- <?php include('./components/Header/header.php'); ?>
+    <?php include './Components/Header/Header.php'; ?>
+
 
     <!-- Jquery min Files -->
     <script src="./Components/Dist/Jquery-3.1.1.min.js"></script>
     <!-- Sematic Min Files -->
-    <script src="./semantic.min.js"></script>
+    <script src="./Components/Dist/semantic.min.js"></script>
+    <!-- Custom-Js-Files -->
+    <script src="./Components/Dist/Custom.js"></script>
 </body>
-</html>
+
+</html> 
